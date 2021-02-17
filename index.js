27a173fbaf9dc96c2e2876859e2cbd648ea431eb
SethMacP <macpherson.seth@gmail.com>
2021-02-16 16:15:42 -0800

    Index set up with server & listen

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require("server");
+const port = 8000;
+
+server.listen(port, ()=>{
+    console.log(`Server running at http://localhost:${port}`)
+})
